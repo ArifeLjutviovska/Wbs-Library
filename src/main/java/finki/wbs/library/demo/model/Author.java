941e6359e0fd2ae9a8bba2f5e1df49b0901ee8f6
ArifeLjutviovska <56047137+ArifeLjutviovska@users.noreply.github.com>
2020-09-29 16:04:52 +0200

    Add files via upload

--- src/main/java/finki/wbs/library/demo/model/Author.java
+++ src/main/java/finki/wbs/library/demo/model/Author.java
@@ -5,20 +5,28 @@ import java.util.List;
 public class Author {
 
     private String Name;
+    private String Label;
     private List<String> hasCreated;
     private String type; //organization or person
 
     public Author() {
     }
 
-    public Author(String name, List<String> hasCreated, String type) {
+    public Author(String name, List<String> hasCreated, String type,String Label) {
         Name = name;
         this.hasCreated = hasCreated;
         this.type = type;
+        this.Label=Label;
     }
 
 
+    public String getLabel() {
+        return Label;
+    }
 
+    public void setLabel(String label) {
+        Label = label;
+    }
 
     public String getName() {
         return Name;
--- src/main/java/finki/wbs/library/demo/model/Contributor.java
+++ src/main/java/finki/wbs/library/demo/model/Contributor.java
@@ -5,20 +5,28 @@ import java.util.List;
 public class Contributor {
 
     private String Name;
+    private String Label;
     private List<String> hasContributed;
     private String type; //organization or person
 
     public Contributor() {
     }
 
-    public Contributor(String name, List<String> hasContributed, String type) {
+    public Contributor(String name, List<String> hasContributed, String type,String Label) {
         Name = name;
         this.hasContributed = hasContributed;
         this.type = type;
+        this.Label=Label;
 
     }
 
+    public String getLabel() {
+        return Label;
+    }
 
+    public void setLabel(String label) {
+        Label = label;
+    }
 
     public String getName() {
         return Name;
